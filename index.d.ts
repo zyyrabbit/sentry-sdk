c368822a6a5847e465c282dfbe154158a274d3af
fee <fee@MacBook-Pro-2.local>
2020-11-05 16:24:44 +0800

    refactor: 重构项目

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,14 @@
 import { BrowserOptions } from '@sentry/browser'
 
+type Options = BrowserOptions & {
+  Vue: any
+}
+
 declare class Report {
-    constructor(options: BrowserOptions)
-    log(exception: Error, data: any): void
+  static install(Vue, options: Options): void
+  static init(options: Options): void
+  static registerError(): void
+  static log(exception: Error, data: any): void
 }
 
 export default Report
--- index.js
+++ index.js
@@ -1,49 +1,62 @@
-import Vue from 'vue'
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
 class Report {
-  options;
-
-  constructor(options = {}) {
-    this.options = options;
-    this.init();
-    this.registerError();
+  /**
+   * 用于Vue 插件加载问题
+   * @param {*} Vue 
+   * @param {*} options 
+   */
+  static install(Vue, options = {}) {
+    options.Vue = Vue;
+    Report.init(options);
   }
-
-  init () {
+  
+  /**
+   * 初始化函数
+   * @param {*} options 
+   */
+  static init (options = {}) {
     // sentry初始化参数
-    Sentry.init({
-      ...this.options,
-      integrations: [
-        new Integrations.Vue( { Vue, attachProps: true,  logErrors: true }),
-      ],
-    });
-    Vue.prototype.$report = this;
+    if (options.Vue) {
+      options.integrations = [
+        new Integrations.Vue({ 
+          Vue: options.Vue, 
+          attachProps: true,  
+          logErrors: true 
+        }),
+      ];
+    }
+    Sentry.init(options);
+    Report.registerError();
   }
-
-  registerError () {
+  /**
+   * 处理资源加载失败异常
+   */
+  static registerError () {
     window.addEventListener('error', (e) => {
       if (!e.target) return;
       const typeName = e.target.tagName.toLowerCase();
       if (typeName === 'link' || typeName === 'script') {
         const scoureUrl = e.target.href || e.target.src;
-        this.log(e, {
+        Report.log(e, {
           desc: `资源加载失败：${scoureUrl}`,
           scoureUrl
         });
       }  
     }, true);
   }
-
-  log (exception, data) {
+  /**
+   *  手动上传错误日志
+   * @param {*} exception @ Exception
+   * @param {*} extra 
+   */
+  static log (exception, extra = {}) {
+    extra.date = new Date();
     Sentry.captureException(exception, {
       level: Sentry.Severity.Error,
-      extra: {
-        ...data,
-        date: new Date()
-      }
-    })
+      extra
+    });
   }
 
 }
